f9240148b521a0ceef85887e93ba743d41b19c65
Daniel Sanchez <danielsanchez1502@gmail.com>
2022-01-16 11:17:58 -0300

    container-styles for last view

--- src/container/App.jsx
+++ src/container/App.jsx
@@ -1,10 +1,14 @@
 import React from 'react'
+import Feedback from '../pages/Feedback';
 import Welcome from '../pages/Welcome';
 
 
 const App = () => {
     return (
-        <Welcome />
+        <>
+        <Feedback />
+        {/* <Welcome /> */}
+        </>
     )
 }
 
--- src/pages/Feedback.jsx
+++ src/pages/Feedback.jsx
@@ -1,11 +1,25 @@
-import React from 'react'
+import React from "react";
 
 const Feedback = () => {
-  return (
-    <div>
-      
-    </div>
-  )
-}
+	return (
+		<div className="wrapper w-100 vh-100">
+			<div className="container-sm h-100 p-4 d-flex flex-column align-items-center ">
+        div
+				<div id="hero" className="container-sm">
+					<img
+						className="d-block mx-auto mb-4"
+						src="https://res.cloudinary.com/dckunlwcb/image/upload/v1642290338/Bootstrap/last-pic_kxwvo3.svg"
+						alt="Initial-image"
+						width="280"
+						heigh="280"
+					/>
+					<p className=" end-text text-center">
+          ¡Excelente! ya eres parte de este producto genial!.
+					</p>
+				</div>
+			</div>
+		</div>
+	);
+};
 
-export default Feedback
+export default Feedback;
